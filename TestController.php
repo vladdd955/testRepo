9442dbcc4ff2d244a462a22b5fa07458ee01960c
vladdd995 <vlad27@inbox.lv>
2024-12-20 05:49:50 +0200

    check

---
+++ TestController.php
@@ -0,0 +1,24 @@
+<?php
+
+class TestController
+{
+
+    public function testAction()
+    {
+        $x = 2;
+        $y = 3;
+
+        $res = $x + $y;
+
+
+
+        return $res;
+    }
+
+
+    public function test2()
+    {
+        $rr = $this->testAction();
+
+    }
+}
